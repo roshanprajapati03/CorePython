de2cf0223ba43e9f2fc8774272372937d483eb38
Roshan Prajapati <instaboy207@hotmail.com>
2021-01-20 11:20:22 +0530

    Add files via upload

---
+++ Chapter 1 - Writing Our First Python Program/Writing Our First Python Program.py
@@ -0,0 +1,34 @@
+# first program to add two numbers
+ 
+a = 10 # store 10 into variable a - line 1
+b = 25 # store 25 into variable b - line 2 
+c = a + b # add 10 and 25 and store into variable c - line 3  
+print("Sum= ", c) # display the sum - line 4 
+
+# README 
+# The hash '#' symbol represents the comments line in Python. 
+# memory location are called 'variable'. In the second line of our program, we stored
+# the data into a variable (or memory location) by the name 'a' as:
+# a = 10
+
+# similarly, in the 2 line, we stored data 25 into another variable 'b' as:
+# b = 25 
+
+# In the 3 line, we added another by writing 'a + b' The result pf this sum should be 
+# stored into another variable (or memory location) by the name 'c' as:
+# c = a + b 
+
+# the result is in 'c' We should now display the result on the screen for the user.
+# This is done by print() function. The purpose of a function is to perfrom a Task
+# print(c)
+# this will display the result 35. But we went to display some message.
+# sum = 35
+
+# this will help the user to understand the result clearly. For this purpose, we used 
+# the print() function in 4 line as:
+# print("Sum = ", c)
+
+# here, "Sum= " is call a string. A string contains a group of character and is usually 
+# enclosed in double quotes or single quotes. String are displayed without any change
+# as they are after this string, we put a comma and then 'c' this will display the result as:
+# sum = 35 
